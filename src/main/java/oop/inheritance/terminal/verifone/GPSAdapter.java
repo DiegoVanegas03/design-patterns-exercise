1656eaf53014fce74ee9bb5c6b5624e75760585d
DiegoVanegas03 <telediego18@gmail.com>
2022-12-01 14:42:16 -0600

    Refactorizacion Codigo

---
+++ src/main/java/oop/inheritance/terminal/verifone/GPSAdapter.java
@@ -0,0 +1,32 @@
+package oop.inheritance.terminal.verifone;
+
+import oop.inheritance.terminal.GPS;
+import oop.library.ingenico.model.Transaction;
+import oop.library.v240m.VerifoneV240mGPS;
+
+public class GPSAdapter implements GPS {
+    private final VerifoneV240mGPS verifoneV240mGPS;
+
+    public GPSAdapter() {
+        verifoneV240mGPS = new VerifoneV240mGPS();
+    }
+
+    @Override
+    public void open() {
+        verifoneV240mGPS.open();
+    }
+
+    @Override
+    public void send(Transaction transaction) {
+    }
+
+    @Override
+    public byte[] receive() {
+        return verifoneV240mGPS.receive();
+    }
+
+    @Override
+    public void close() {
+        verifoneV240mGPS.close();
+    }
+}
